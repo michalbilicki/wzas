3ba6d597150de04010177fa62cbc017f3f86db68
Radosław Łapciak <31709862+Sorcuss@users.noreply.github.com>
2020-05-10 21:35:30 +0200

    Delete MethodSecurityConfiguration.java

--- src/main/java/cyber/punks/wzas/auth/configuration/MethodSecurityConfiguration.java
+++
@@ -1,8 +0,0 @@
-package cyber.punks.wzas.auth.configuration;
-
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
-
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
-public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
-}
